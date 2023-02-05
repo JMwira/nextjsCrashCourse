142d1c6b21fd646d11b1a0a60c26761af5ac7e84
JMwira <jaymwira@gmail.com>
2023-02-05 13:12:39 +0200

    trial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,9 +13,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>Welcome to next js</p>
+      <main>
+        <div>
+          <p className='text-3xl' >Welcome to next js</p>
         </div>
       </main>
     </>
